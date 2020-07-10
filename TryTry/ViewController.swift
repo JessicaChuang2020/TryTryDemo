82b80dbe8b5adee350727364cb3436ff7bab9c70
stoller <stoller@stollerde-macbook-pro.local>
2020-07-10 23:16:32 +0800

    Initial Commit

---
+++ TryTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryTry
+//
+//  Created by stoller on 2020/7/10.
+//  Copyright © 2020年 yoyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
